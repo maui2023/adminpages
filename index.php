355d6f0babcaa71c1d67cb9a1c15bcb686da003b
maui2023 <mauisabily@gmail.com>
2022-06-20 18:05:57 +0800

    laman utama

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<meta http-equiv="refresh" content="3;url=./welcome.php" />
+<title>Page Moved</title>
+</head>
+<body>
+Laman Asas Tanpa Log Masuk. Tekan Di Sini <a href="./welcome.php">Laman Utama</a>.
+</body>
+</html>
